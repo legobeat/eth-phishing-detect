f5e52e687b80a85286ec71e0ceaa7fdc15a889ed
legobt <6wbvkn0j@anonaddy.me>
2022-11-04 09:47:47 +0000

    WIP: add duplicate validation to add-hosts

--- src/add-hosts.js
+++ src/add-hosts.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const config = require('./config');
+const PhishingDetector = require('./detector')
 
 const VALID_LIST_NAMES = [
   'blacklist', 'fuzzylist','whitelist'
@@ -36,6 +37,45 @@ const hosts = process.argv.slice(3);
 
 if (!VALID_LIST_NAMES.includes(list) || hosts.length < 1) {
   exitWithUsage(1);
+}
+
+const detector = new PhishingDetector(config);
+
+let allNew = true;
+switch (list) {
+  case 'blacklist':
+    for (let i=0; i < hosts.length; i++) {
+      const h = hosts[i];
+      const r = detector.check(h);
+      if (r.result) {
+        console.error(`'${h}' already covered by '${r.match}' in '${r.type}'.`);
+        allNew = false;
+      }
+    }
+    break;
+  case 'whitelist':
+    for (let i=0; i < hosts.length; i++) {
+      const h = hosts[i];
+      const r = detector.check(h);
+      if (!r.result) {
+        console.error(`'${h}' does not require whitelisting`);
+        allNew = false;
+      }
+    }
+    break;
+  case 'fuzzylist':
+    const alreadyIncluded = hosts.filter(h => config.fuzzylist.includes(h));
+    if (alreadyIncluded.length) {
+      console.error(`'${alreadyIncluded.join(',')}' are already in fuzzylist`);
+      allNew = false;
+    }
+  default:
+    exitWithUsage(1);
+}
+// todo:  check if  no internal duplicates (generate new config>remove entry>check)
+
+if (!allNew) {
+  process.exit(1);
 }
 
 addHosts(list, hosts, './src/config.json');
